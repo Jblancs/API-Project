5155ae1d25f4e6ceff662fd78aa94704c14352e6
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-01-25 08:29:55 -0500

    reviews data seeders added

---
+++ backend/db/seeders/20230125132438-reviews-data.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'Reviews';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 2,
+        review: 'great place with nice view',
+        stars: 4
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        review: 'would come back again',
+        stars: 4
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        review: 'amazing host!',
+        stars: 5
+      }
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
