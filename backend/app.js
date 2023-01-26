891a86241bd22c7b294fce4103bb4d22f6505b15
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-01-26 11:57:43 -0500

    create spot completed

--- backend/app.js
+++ backend/app.js
@@ -72,6 +72,7 @@ app.use((err, _req, res, _next) => {
     res.json({
         title: err.title || 'Server Error',
         message: err.message,
+        statusCode: res.statusCode,
         errors: err.errors,
         stack: isProduction ? null : err.stack
     });
