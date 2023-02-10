01524aba02c5e6884d0c2d9aa858a48ee20d977a
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-02-10 18:04:38 -0500

    phase 2 signup action completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,7 @@ const removeUser = () => {
     };
 };
 
+// login thunk
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
@@ -30,8 +31,27 @@ export const login = (user) => async (dispatch) => {
     return response;
 };
 
+// restore user thunk
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+// signup thunk
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
