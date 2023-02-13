7bfe71e5049a3acc63b141c3f9bc375b8b6bfba1
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-02-13 10:27:18 -0500

    scripts in root package.json file updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,13 +10,13 @@ const apiRouter = require('./api');
 // });
 
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
+// router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//         'XSRF-Token': csrfToken
+//     });
+// });
 
 router.use('/api', apiRouter);
 
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
     });
 }
 
