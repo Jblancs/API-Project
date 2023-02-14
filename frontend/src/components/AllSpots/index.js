33837da8a347c05ccb12d428cccc42a5f2246390
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-02-14 10:51:55 -0500

    all spots cards and css formatting in progress

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { Link } from "react-router-dom"
 import { getAllSpots } from "../../store/spotsReducer"
+import "./index.css"
 
 const AllSpots = () => {
     const allSpotsObj = useSelector(state => state.spots.allSpots)
@@ -19,11 +20,26 @@ const AllSpots = () => {
 
     return (
         <div className="spots">
-            {allSpotsArr && allSpotsArr.map(spot => (
-                <Link key={spot.id} to={`/spots/${spot.id}`}>
-                    {spot.previewImage}
-                </Link>
-            ))}
+            <div className="spots__container">
+                {allSpotsArr && allSpotsArr.map(spot => (
+                    <div className="spots__prev">
+                        <Link key={`link${spot.id}`} to={`/spots/${spot.id}`}>
+                            <img key={`img${spot.id}`} className="spots__prev__img" src={spot.previewImage} />
+                        </Link>
+                        <div className="spots__prev__locrate">
+                            <div key={`loc${spot.id}`} className="spots__prev__loc">
+                                {spot.city}, {spot.state}
+                            </div>
+                            <div key={`rate${spot.id}`} className="spots__prev__rate">
+                                <i className="fa-solid fa-star" />{spot.avgRating}
+                            </div>
+                        </div>
+                        <div key={`price${spot.id}`} className="spots__prev__price">
+                            ${spot.price} night
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
