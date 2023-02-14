c76a9da4cf09c4ff382cdb96e65e0afd5f0036b2
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-02-14 09:05:19 -0500

    spot image seeders updated

--- backend/db/seeders/20230124233631-spotImages-data.js
+++ backend/db/seeders/20230124233631-spotImages-data.js
@@ -13,33 +13,63 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/259950/pexels-photo-259950.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         preview: true
       },
       {
         spotId: 2,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         preview: true
       },
       {
         spotId: 3,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         preview: true
       },
       {
         spotId: 4,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         preview: true
       },
       {
         spotId: 5,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/1732414/pexels-photo-1732414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         preview: true
       },
       {
         spotId: 6,
-        url: 'image url',
+        url: 'https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg',
         preview: true
+      },
+      {
+        spotId: 1,
+        url: 'https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://images.pexels.com/photos/2988860/pexels-photo-2988860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://images.pexels.com/photos/280232/pexels-photo-280232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://images.pexels.com/photos/3554424/pexels-photo-3554424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://images.pexels.com/photos/2440471/pexels-photo-2440471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: false
       }
     ], {});
   },
