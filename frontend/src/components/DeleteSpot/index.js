630e63ce3c007a064b836f20dae196e0b22fc790
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-02-17 11:10:39 -0500

    delete spot functional

--- frontend/src/components/DeleteSpot/index.js
+++ frontend/src/components/DeleteSpot/index.js
@@ -1,16 +1,36 @@
+import { useDispatch } from "react-redux"
 import { useModal } from "../../context/Modal"
+import { deleteUserSpot, getCurrentSpots } from "../../store/spotsReducer"
+import "./index.css"
 
-function DeleteSpot() {
+function DeleteSpot({ spotId, setDeletedSpot }) {
     const { closeModal } = useModal()
+    const dispatch = useDispatch()
 
-    const clickHandler = (e) => {
+    const clickHandlerYes = async (e) => {
         e.preventDefault()
-        // return closeModal()
+
+        const removeSpot = await dispatch(deleteUserSpot(spotId))
+        const reRender = await dispatch(getCurrentSpots(spotId))
+
+        return closeModal()
+    }
+
+    const clickHandlerNo = (e) => {
+        e.preventDefault()
+        return closeModal()
     }
 
     return (
-        <div>
-            <button onClick={clickHandler}>No(Keep Spot)</button> 
+        <div className="delete-container">
+            <div className="delete-div">
+                <h3 className="delete-header">Confirm Delete</h3>
+                <div className="delete-text">
+                    Are you sure you want to remove this spot from the listings?
+                </div>
+                <button onClick={clickHandlerYes} className="delete-btn-yes">Yes (Delete Spot)</button>
+                <button onClick={clickHandlerNo} className="delete-btn-no">No (Keep Spot)</button>
+            </div>
         </div>
     )
 }
