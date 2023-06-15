61eb182fb05f2b71803ae3278b93b652dfaad9f3
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-06-14 22:44:10 -0400

    updated logo and nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
-import pearHouse from './images/pear-house.png'
+import pearHouse from './images/pear-logo.png'
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
@@ -43,7 +43,8 @@ function Navigation({ isLoaded }) {
     }
 
     return (
-        <nav className='nav__bar'>
+        <nav className='nav__container'>
+            <div className='nav__bar'>
             <NavLink exact to="/" style={{ textDecoration: 'none' }}>
                 <div className='nav__logo'>
                     <img className="nav__logo__img" src={pearHouse} alt="pearImage" />
@@ -53,6 +54,7 @@ function Navigation({ isLoaded }) {
             <div className='nav__buttons'>
                 {isLoaded && sessionLinks}
             </div>
+        </div>
         </nav>
     );
 }
